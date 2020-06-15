2ca513be4df98407a7e228762a1a14dfa7b47aef
d-novitskiy <you@example.com>
2020-06-15 21:30:39 +0300

    add first and last btn for pagination

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -4,7 +4,7 @@ import styles from './styles.module.css';
 export function Pagination({
   moviesPerPage, total, paginate,
 }) {
-  const pagesAmount = 15;
+  const pagesAmount = 10;
   const NumberOfPages = Math.ceil(total / moviesPerPage);
   const [active, setActive] = useState(1);
   const pages = {
@@ -30,12 +30,14 @@ export function Pagination({
   return (
     <div className={styles.container}>
       <ul>
+        <li><button disabled={!(pageNumber > 1)} id="first" type="button" className={styles.btn} onClick={() => setPageNumber(1)}>{'<<'}</button></li>
         <li><button disabled={!(pageNumber > 1)} id="prev" type="button" className={styles.btn} onClick={() => setPageNumber(pageNumber - 1)}>{'<'}</button></li>
         {pages.pages.filter((page) => page >= leftBorderNumber && page <= rightBorderNumber)
           .map((item) => {
             return <li><button type="button" id={item} onClick={paginateHandler} className={`${styles.btn} ${pages.active === item ? styles.active : ''}`}>{item}</button></li>;
           })}
         <li><button disabled={!(pagesCount > pageNumber)} id="next" type="button" className={styles.btn} onClick={() => setPageNumber(pageNumber + 1)}>{'>'}</button></li>
+        <li><button disabled={!(pagesCount > pageNumber)} id="last" type="button" className={styles.btn} onClick={() => setPageNumber(pagesCount)}>{'>>'}</button></li>
       </ul>
     </div>
   );
