508bb886e0797537ab8369441e2652942a236a21
d-novitskiy <you@example.com>
2020-06-22 23:03:42 +0300

    Updates

---
+++ precache-manifest.a2925c8076abf3ab4aeec1c395cc5c5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d119afccedfcb03bd407a28314d9e19a",
+    "url": "/movies/index.html"
+  },
+  {
+    "revision": "c21fd6834e7b16dcd6b1",
+    "url": "/movies/static/css/main.f22846ad.chunk.css"
+  },
+  {
+    "revision": "0d04a95274aec33589c2",
+    "url": "/movies/static/js/2.38f4fa51.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movies/static/js/2.38f4fa51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c21fd6834e7b16dcd6b1",
+    "url": "/movies/static/js/main.4a78b9c6.chunk.js"
+  },
+  {
+    "revision": "1241a641777761abfc2e",
+    "url": "/movies/static/js/runtime-main.daa07c4d.js"
+  },
+  {
+    "revision": "b97832d903bbe3cdd3571ef59a713e05",
+    "url": "/movies/static/media/header.b97832d9.jpg"
+  }
+]);
